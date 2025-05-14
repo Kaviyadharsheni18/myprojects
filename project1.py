9436d01d34aef35bc8e462e8facc50676b792df4
Kaviyadharsheni18 <kaviyadharsheni10b@gmail.com>
2025-05-14 18:53:43 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,14 +3,15 @@
 
 print("Welcome to the game")
 print("Guess a  no. between 1 t0 10.")
-x = int(input("Enter the number:"))
+
 guess = 5
 while True:
+   x = int(input("Enter the number:"))
    if (guess == x):
       print("Yay! You have guess it right")
       break
+      
    else:
       print("Good luck next time!") 
-      break
 
 
