865a79532dbf2a0a1c4121b48e84631b59c1fb29
Kaviyadharsheni18 <kaviyadharsheni10b@gmail.com>
2025-05-20 12:57:06 +0530

    project1 is modified

--- project1.py
+++ project1.py
@@ -1,19 +1,29 @@
-#Beginner project
+#Beginner level projects
 # #Guess the Number game
+#first
 
 print("Welcome to the game")
-print("Guess a  no. between 1 t0 10.")
+print("Guess a  no. between 1 t0 100.")
 
-guess = 5
-while True:
-   x = int(input("Enter the number between 1 to 10:"))
-   
-   if (guess == x):
-      print("Yay! You have guess it right")
-      break
-      
-   else:
-      print("Good luck next time!")
+guess = 12
+playing = True
+while playing:
+    x = int(input('Enter a number between 1 to 100:'))
+    if x < guess:
+        print('Your guess is lesser than the answer')
+        continue
+     
+    elif x > guess:
+        print('Your guess is larger than the answer')
+        continue
+    elif x == guess:
+        print('Yay You have guessed it right')
+        break
+    else:
+        print('Invalid choice')
+
+#second
+
       
     
 
