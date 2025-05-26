ab455a15e07a1bbfe4fc9cb7e19af72f88e8af29
Kaviyadharsheni18 <kaviyadharsheni10b@gmail.com>
2025-05-26 12:47:03 +0530

    changes added

--- project1.py
+++ project1.py
@@ -1,30 +1,7 @@
 #Beginner level projects
-# #Guess the Number game
-#first
-
-print("Welcome to the game")
-print("Guess a  no. between 1 t0 100.")
-
-guess = 12
-playing = True
-while playing:
-    x = int(input('Enter a number between 1 to 100:'))
-    if x < guess:
-        print('Your guess is lesser than the answer')
-        continue
-     
-    elif x > guess:
-        print('Your guess is larger than the answer')
-        continue
-    elif x == guess:
-        print('Yay You have guessed it right')
-        break
-    else:
-        print('Invalid choice')
-
-#second
-#game 2
+#game 1
 #Roll a dice game
+
 import random
 choice = input("Do you want to play the game(y/n):").lower()
 playing = True
@@ -44,14 +21,28 @@ while playing:
       print("Invalid choice.")
       break
 
-      
+
+
+#game 2 
+#Number guessing game
+import random
+
+guess = random.randint(1,100)
+
+while True:
+    try:
+       x = int(input("Guess a number between 1 to 100:"))
+
+       if x > guess:
+          print("Too high")
+       elif x < guess:
+          print("Too less")
+       else:
+          print("Yay! You guess it right")
+    except ValueError:
+       print("please enter a valid number:")
     
 
-      
-     
- 
+        
 
-     
-   
 
-              
